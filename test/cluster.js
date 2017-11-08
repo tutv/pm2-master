c7cbcad23ffd134e67b7c4fe9da182865070e651
Max <tutv95@gmail.com>
2017-11-08 10:54:40 +0700

    Updated cluster

--- test/cluster.js
+++ test/cluster.js
@@ -1,20 +1,9 @@
 const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
 const pm2Master = require('../index');
 
 const assert = require('assert');
-const Assertion = require('should');
 
 describe('Clustering.', () => {
-    before(() => {
-        if (cluster.isMaster) {
-            // Fork workers.
-            for (let i = 0; i < numCPUs; i++) {
-                cluster.fork();
-            }
-        }
-    });
-
     it('should return true', () => {
         if (cluster.isMaster) {
             assert(true, pm2Master.isMaster());
